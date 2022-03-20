b3bbb913d3e5624adf184d9c5c04e29baa46ef8f
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-03-20 15:00:05 +0100

    can drag and drop windows

--- src/Components/Windows.tsx
+++ src/Components/Windows.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { motion } from "framer-motion"
 import WindowManager from "../reducers/useWindows";
 import Window from "./Window";
+import "./Windows.css";
 
 function Windows() {
   const { contentsState, openWindow, closeWindow } = WindowManager.useContainer();
+  const constraintsRef = useRef(null);
+
   return (
-    <>
+    <div className="windows-container">
+      <motion.div className="drag-area" ref={constraintsRef} />
     {
-      contentsState.map(({url, show, name}, index) => (
-        show && <Window key={index} label={name} maximize={()=> openWindow(index)} minimize={()=> closeWindow(index)} publicUrl={url} />
+      contentsState.map(({url, show, name, left}, index) => (
+        show && 
+        <motion.div className="dot" drag dragConstraints={constraintsRef}>
+        <Window
+          key={index}
+          label={name}
+          maximize={()=> openWindow(index)}
+          minimize={()=> closeWindow(index)}
+          publicUrl={url}
+        />
+        </motion.div>
        )
       )
     }
-    </>
+    </div>
   );
 }
 
