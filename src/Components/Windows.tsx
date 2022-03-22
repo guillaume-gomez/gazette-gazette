267f9a44f63a582e6ca16a4087529fcce314bbd2
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-03-22 09:14:55 +0100

    conditional rendering based in breakpoints

--- src/Components/Windows.tsx
+++ src/Components/Windows.tsx
@@ -1,32 +1,55 @@
 import React, { useRef } from 'react';
 import { motion } from "framer-motion"
 import WindowManager from "../reducers/useWindows";
+import useMediaQuery from "../reducers/useMediaQuery"
+
 import Window from "./Window";
 import "./Windows.css";
 
 function Windows() {
   const { contentsState, toggleShow, toggleFullscreen } = WindowManager.useContainer();
+  const isMedium = useMediaQuery('(max-width: 768px)');
   const constraintsRef = useRef(null);
 
-  return (
-    <div className="windows-container">
-      <motion.div className="drag-area" ref={constraintsRef} />
-    {
-      contentsState.map( (contentState, index) => (
-        contentState.show &&
-        <motion.div className="dot" drag dragConstraints={constraintsRef}>
-        <Window
-          key={index}
-          toggleShow={()=> toggleShow(index)}
-          toggleFullscreen={()=> toggleFullscreen(index)}
-          windowContent={contentState}
-        />
-        </motion.div>
-       )
-      )
-    }
-    </div>
-  );
+  if(isMedium) {
+    return (
+      <div className="windows-container">
+        {
+          contentsState.map( (contentState, index) => (
+            contentState.show &&
+                  <Window
+                    key={index}
+                    toggleShow={()=> toggleShow(index)}
+                    toggleFullscreen={()=> toggleFullscreen(index)}
+                    windowContent={contentState}
+                  />
+            )
+          )
+        }
+      </div>
+    );
+  } else {
+    return (
+      <div className="windows-container">
+        <motion.div className="drag-area" ref={constraintsRef} />
+      {
+        contentsState.map( (contentState, index) => (
+          contentState.show &&
+          <motion.div className="dot" drag dragConstraints={constraintsRef}>
+          <Window
+            key={index}
+            toggleShow={()=> toggleShow(index)}
+            toggleFullscreen={()=> toggleFullscreen(index)}
+            windowContent={contentState}
+          />
+          </motion.div>
+         )
+        )
+      }
+      </div>
+    );
+  }
+
 }
 
 export default Windows;
---
+++ src/reducers/useMediaQuery.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react"
+
+function useMediaQuery(query : string) {
+  const [matches, setMatches] = useState<boolean>(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => {
+      setMatches(media.matches);
+    };
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [matches, query]);
+
+  return matches;
+}
+
+export default useMediaQuery;
