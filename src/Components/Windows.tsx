4168424af0ff12a2e74e74b45f0c363cf2bc7cf5
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-03-26 04:26:36 +0100

    windows add random

--- src/Components/Windows.tsx
+++ src/Components/Windows.tsx
@@ -6,6 +6,14 @@ import useMediaQuery from "../reducers/useMediaQuery"
 import Window from "./Window";
 import "./Windows.css";
 
+const variants = {
+  initial: { opacity: 0},
+  visible: (custom: number) => ({
+    opacity: 1,
+    transition: { delay: custom * 0.5 }
+  })
+}
+
 function Windows() {
   const { contentsState, changeWindowState } = WindowManager.useContainer();
   const isMedium = useMediaQuery('(max-width: 768px)');
@@ -33,12 +41,14 @@ function Windows() {
       <motion.div className="drag-area" ref={constraintRef} >
       {
         contentsState.map( (contentState, index) => (
-            <Window
-              key={index}
-              changeWindowState={(value)=> changeWindowState(index, value)}
-              windowContent={contentState}
-              dragConstraints={constraintRef}
-            />
+            <motion.span  style={{width: "100%", height:"100%"}} initial="initial" custom={index} animate="visible" variants={variants} >
+              <Window
+                key={index}
+                changeWindowState={(value)=> changeWindowState(index, value)}
+                windowContent={contentState}
+                dragConstraints={constraintRef}
+              />
+            </motion.span>
 
          )
         )
